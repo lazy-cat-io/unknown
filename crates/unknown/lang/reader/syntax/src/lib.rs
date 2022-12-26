4f80cd580465d6649cb46ab08722bc3667a9baea
Ilshat Sultanov <ilshat@sultanov.team>
2022-12-27 01:34:57 +0300

    Init `unknown-syntax` crate

---
+++ crates/unknown/lang/reader/syntax/src/lib.rs
@@ -0,0 +1,50 @@
+use std::mem;
+
+pub type SyntaxBuilder = eventree::SyntaxBuilder<TreeConfig>;
+pub type SyntaxElement = eventree::SyntaxElement<TreeConfig>;
+pub type SyntaxNode = eventree::SyntaxNode<TreeConfig>;
+pub type SyntaxToken = eventree::SyntaxToken<TreeConfig>;
+pub type SyntaxTree = eventree::SyntaxTree<TreeConfig>;
+pub type Event = eventree::Event<TreeConfig>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TreeConfig {}
+
+impl eventree::TreeConfig for TreeConfig {
+    type NodeKind = NodeKind;
+    type TokenKind = TokenKind;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TokenKind {
+    Whitespace,
+    Comma,
+    Boolean,
+    Error,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum NodeKind {
+    Root,
+    Error,
+}
+
+unsafe impl eventree::SyntaxKind for TokenKind {
+    fn to_raw(self) -> u16 {
+        self as u16
+    }
+
+    unsafe fn from_raw(raw: u16) -> Self {
+        mem::transmute(raw as u8)
+    }
+}
+
+unsafe impl eventree::SyntaxKind for NodeKind {
+    fn to_raw(self) -> u16 {
+        self as u16
+    }
+
+    unsafe fn from_raw(raw: u16) -> Self {
+        mem::transmute(raw as u8)
+    }
+}
